02e1b726ad7a8ad90de6aebdf9b62b8dd7157fc0
Victor-Estrella <victorhcarracci@gmail.com>
2024-09-03 10:39:19 -0300

    ss

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,58 +4,56 @@ import "./style.css";
 
 export default function Footer() {
   return (
-    <footer>
-        <div className="main-content">
-            <div className="esquerda box">
-                <h2>Contato</h2>
-                <div className="content">
+    <footer className="w-full bg-[#111]">
+        <div className="flex text-cinza">
+            <div className="esquerda basis-1/2 py-2.5 px-5">
+                <h2 className="text-lg font-semibold uppercase">Contato</h2>
+                <div className="relative mx-auto align-center py-0 px-5 mt-5 before:absolute before:content-[''] before:w-full before:bg-[#1a1a1a] before:h-0.5 before:top-[-10px] after:absolute after:content-[''] after:w-1/5 after:bg-[#020fff] after:h-0.5 after:top-[-10px]">
                     <form action="#">
-                        <div className="nome">
-                            <div className="text">Nome</div>
+                        <div className="mt-7 nome">
+                            <div className="mt-7 font-semibold text-[#202020] text-[1.5rem]">Nome</div>
                             <input type="text" required />
                         </div>
-                        <div className="email">
-                            <div className="text">E-mail</div>
+                        <div className="mt-7 email">
+                            <div className="mt-7 font-semibold text-[#202020] text-[1.5rem]">E-mail</div>
                             <input type="email" required />
                         </div>
-                        <div className="msg">
-                            <div className="text">Mensagem (explique seu problema)</div>
+                        <div className="mt-7 msg">
+                            <div className="mt-7 font-semibold text-[#202020] text-[1.5rem]">Mensagem (explique seu problema)</div>
                             <textarea rows={2} cols={25} required></textarea>
                         </div>
-                        <div className="btn-submit">
+                        <div className="mt-7 btn-submit">
                             <button type="submit">Enviar</button>
                         </div>
                     </form>
                 </div>
             </div>
-            <div className="direita box">
-                <h2>Endereço</h2>
-                <div className="content">
-                    <div className="lugar">
-                        <span className="text">R EMILIA MARENGO, 451 - VILA REGENTE FEIJO - SAO PAULO</span>
+            <div className="direita basis-1/2 py-2.5 px-5">
+                <h2 className="text-lg font-semibold uppercase">Endereço</h2>
+                <div className="relative mx-auto align-center py-0 px-5 mt-5 before:absolute before:content-[''] before:w-full before:bg-[#1a1a1a] before:h-0.5 before:top-[-10px] after:absolute after:content-[''] after:w-1/5 after:bg-[#020fff] after:h-0.5 after:top-[-10px]">
+                    <div className="mt-5 lugar">
+                        <span className="font-semibold text-[#202020] text-[1.5rem]">R EMILIA MARENGO, 451 - VILA REGENTE FEIJO - SAO PAULO</span>
                     </div>
-                    <div className="phone">
-                        <span className="text">+333 76786</span>
+                    <div className="mt-5 phone">
+                        <span className="font-semibold text-[#202020] text-[1.5rem]">+333 76786</span>
                     </div>
-                    <div className="email">
-                        <span className="text">prevencao.cartoes@portoseguro.com.br.</span>
+                    <div className="mt-5 email">
+                        <span className="font-semibold text-[#202020] text-[1.5rem]">prevencao.cartoes@portoseguro.com.br.</span>
                     </div>
-                    <div className="parcerias">
-                        <img className="icone" src={LogoPorto} alt="Logo Porto Seguro" />
-                        <img className="icone" src={LogoFiap} alt="Logo FIAP" />
+                    <div className="mt-5 flex justify-evenly">
+                        <img className="mt-8 w-60" src={LogoPorto} alt="Logo Porto Seguro" />
+                        <img className="mt-8 w-60" src={LogoFiap} alt="Logo FIAP" />
                     </div>
                 </div>
             </div>
         </div>
-        <div className="bottom">
-            <center>
-                <span className="credit">
-                    Criado por Descomplica Auto{" "}
-                    |{" "}
-                </span>
-                <span className="far fa-copyright"></span>
-                <span> ® 2024. Todos os direitos reservados</span>
-            </center>
+        <div className="flex justify-center p-1 text-sm bg-[#151515]">
+            <span className="credit text-[#656565]">
+                Criado por Descomplica Auto{" "}
+                |{" "}
+            </span>
+            <span className="far fa-copyright text-[#656565]"></span>
+            <span> ® 2024. Todos os direitos reservados</span>
         </div>
     </footer>
   )
