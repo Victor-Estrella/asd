1786e54c456b93dcd63ea3e128b425e0cb25e96f
Victor-Estrella <victorhcarracci@gmail.com>
2024-09-13 00:37:45 -0300

    asdad

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,14 @@
 import { Route, Routes } from "react-router-dom";
-import Cadastro from "./pages/Cadastro/Cadastro";
 import Equipe from "./pages/Equipe/Equipe";
 import FuncionalidadePagina from "./pages/Funcionalidade/FuncionalidadePagina";
 import AdicionarCarro from "./pages/Adicionar-Carro/Adicionar-Carro";
 import PageNotFound from "./pages/PageNotFound/PageNotFound";
+import CadastroLogin from "./pages/Cadastro/CadastroLogin";
 
 function MainRoutes() {
     return (
         <Routes>
-            <Route path="/" element={<Cadastro />} />
+            <Route path="/" element={<CadastroLogin />} />
             <Route path="/Funcionalidade" element={<FuncionalidadePagina />} />
             <Route path="/Sobre" element={<Equipe />} />
             <Route path="/AdicionarCarro" element={<AdicionarCarro/>} />
